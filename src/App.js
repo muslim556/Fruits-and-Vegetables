8abf08df4be47f566284c46b355844b10fd3b82d
muslim556 <muslimbeknajmiddinov31@gmail.com>
2022-08-17 23:33:15 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar } from "./Components/Navbar";
+import { Footer } from "./Components/Footer";
+
+export function App() {
+  return (
+    <React.StrictMode>
+      <Navbar />
+      <Footer />
+    </React.StrictMode>
+  );
+}
